23a24bfd869ff3a9046ac1d9fcd1e7b033dbabd8
Dark_Venom <subhajitdey1110@gmail.com>
2025-03-05 11:39:58 +0530

    new file initial

--- first.cpp
+++ first.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     cout<<"Hello World";
     cout<<"WE are new here";
-    cout<<"This is a new feature !"';
+    cout<<"This is a new feature !";
+    cout<<"Another new feature !";
 return 0;
 }
